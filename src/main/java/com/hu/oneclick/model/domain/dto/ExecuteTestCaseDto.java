9875d4144dd1ba0034f38b5c117e26202d35ca7d
masiyi <masiyi163163@163.com>
2021-11-24 08:40:23 +0800

    增加字段

--- src/main/java/com/hu/oneclick/model/domain/dto/ExecuteTestCaseDto.java
+++ src/main/java/com/hu/oneclick/model/domain/dto/ExecuteTestCaseDto.java
@@ -1,8 +1,11 @@
 package com.hu.oneclick.model.domain.dto;
 
+import lombok.Data;
+
 /**
  * @author qingyang
  */
+@Data
 public class ExecuteTestCaseDto {
 
     private String testCaseId;
@@ -15,54 +18,7 @@ public class ExecuteTestCaseDto {
 
     private Integer RunDuration;
 
-    public Integer getRunDuration() {
-        return RunDuration;
-    }
+    private String actualResult;
 
-    public void setRunDuration(Integer runDuration) {
-        RunDuration = runDuration;
-    }
-
-    public Integer getStep() {
-        return step;
-    }
-
-    public void setStep(Integer step) {
-        this.step = step;
-    }
-
-
-
-    public String getTestCaseId() {
-        return testCaseId;
-    }
-
-    public void setTestCaseId(String testCaseId) {
-        this.testCaseId = testCaseId;
-    }
-
-    public String getTestCycleId() {
-        return testCycleId;
-    }
-
-    public void setTestCycleId(String testCycleId) {
-        this.testCycleId = testCycleId;
-    }
-
-    public String getTestCaseStepId() {
-        return testCaseStepId;
-    }
-
-    public void setTestCaseStepId(String testCaseStepId) {
-        this.testCaseStepId = testCaseStepId;
-    }
-
-    public Integer getStepStatus() {
-        return stepStatus;
-    }
-
-    public void setStepStatus(Integer stepStatus) {
-        this.stepStatus = stepStatus;
-    }
 
 }
