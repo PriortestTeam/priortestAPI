e86790e788705ab62e49b241e484d6da0f80c5f1
masiyi <masiyi163163@163.com>
2021-11-26 10:39:04 +0800

    子用户管理

--- src/main/java/com/hu/oneclick/controller/user/SubUserController.java
+++ src/main/java/com/hu/oneclick/controller/user/SubUserController.java
@@ -5,6 +5,8 @@ import com.hu.oneclick.model.domain.Project;
 import com.hu.oneclick.model.domain.dto.SubUserDto;
 import com.hu.oneclick.server.service.ProjectService;
 import com.hu.oneclick.server.user.SubUserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,6 +24,7 @@ import java.util.List;
 @RestController
 @RequestMapping("subUser")
 @PreAuthorize("@ps.manageSubUsers()")
+@Api(tags = "子用户管理")
 public class SubUserController {
 
     private final SubUserService subUserService;
@@ -36,32 +39,38 @@ public class SubUserController {
 
 
     @PostMapping("querySubUsers")
+    @ApiOperation("查询子用户")
     public Resp<List<SubUserDto>> querySubUsers(@RequestBody SubUserDto sysUser){
         return  subUserService.querySubUsers(sysUser);
     }
 
     @GetMapping("queryForProjects")
+    @ApiOperation("查询项目")
     public Resp<List<Project>> queryForProjects(){
         return  projectService.queryForProjects();
     }
 
     @PostMapping("createSubUser")
+    @ApiOperation("添加子用户")
     public Resp<String> createSubUser(@RequestBody SubUserDto sysUser){
         return  subUserService.createSubUser(sysUser);
     }
 
     @PostMapping("updateSubUser")
+    @ApiOperation("修改子用户")
     public Resp<String> updateSubUser(@RequestBody SubUserDto sysUser){
         return  subUserService.updateSubUser(sysUser);
     }
 
     @PostMapping("updateSubUserPassword")
+    @ApiOperation("修改子用户密码")
     public Resp<String> updateSubUserPassword(@RequestBody SubUserDto sysUser){
         return  subUserService.updateSubUserPassword(sysUser);
     }
 
 
     @DeleteMapping("deleteSubUser/{id}")
+    @ApiOperation("删除子用户")
     public Resp<String> deleteSubUser(@PathVariable String id){
         return  subUserService.deleteSubUser(id);
     }
