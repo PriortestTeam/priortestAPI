fc9482f09bd975895d2093143d4b874b81048048
juhh <782609572@qq.com>
2023-03-22 20:10:18 +0800

    feat(下拉类型选项): 新增修改下拉选项值接口

--- src/main/java/com/hu/oneclick/controller/CustomFieldsController.java
+++ src/main/java/com/hu/oneclick/controller/CustomFieldsController.java
@@ -6,8 +6,8 @@ import com.hu.oneclick.common.enums.SysConstantEnum;
 import com.hu.oneclick.common.exception.BizException;
 import com.hu.oneclick.model.annotation.Page;
 import com.hu.oneclick.model.base.Resp;
-import com.hu.oneclick.model.domain.CustomFields;
 import com.hu.oneclick.model.domain.dto.CustomFieldDto;
+import com.hu.oneclick.model.domain.dto.CustomFieldsDto;
 import com.hu.oneclick.model.domain.vo.CustomFieldVo;
 import com.hu.oneclick.model.domain.vo.CustomFileldLinkVo;
 import com.hu.oneclick.server.service.CustomFieldsService;
@@ -88,6 +88,15 @@ public class CustomFieldsController {
         }
         log.info("getDropDownBox==>customFieldDto:{}", JSON.toJSONString(customFieldDto));
         return customFieldsService.getDropDownBox(customFieldDto);
+    }
+
+    @PostMapping("/updateValueDropDownBox")
+    public Resp<String> updateValueDropDownBox(@RequestBody CustomFieldsDto customFieldsDto) {
+        if (null == customFieldsDto.getCustomFieldId()) {
+            throw new BizException(SysConstantEnum.PARAM_EMPTY.getCode(), "customFieldId" + SysConstantEnum.PARAM_EMPTY.getValue());
+        }
+        log.info("updateValueDropDownBox==>customFieldsDto:{}", JSON.toJSONString(customFieldsDto));
+        return customFieldsService.updateValueDropDownBox(customFieldsDto);
     }
 
 
