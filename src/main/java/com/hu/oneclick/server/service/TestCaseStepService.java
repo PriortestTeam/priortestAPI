9815a0bbc016b8fed9165b83ed48c7329b641141
xiaohai <seaside5648@163.com>
2023-04-09 13:30:05 +0800

    【新增】新增测试用例步骤增删改查接口

--- src/main/java/com/hu/oneclick/server/service/TestCaseStepService.java
+++ src/main/java/com/hu/oneclick/server/service/TestCaseStepService.java
@@ -14,9 +14,9 @@ public interface TestCaseStepService extends IService<TestCaseStep> {
 
     List<TestCaseStep> list(TestCaseStepParam param);
 
-    TestCaseStep save(TestCaseStepSaveDto dto);
+    void save(TestCaseStepSaveDto dto);
 
-    TestCaseStep update(TestCaseStepSaveDto dto);
+    void update(TestCaseStepSaveDto dto);
 
     TestCaseStep info(Long id);
 
