5f88ad275596e5c34883f72fccd39c85b89d4231
Johnson <905204261@qq.com>
2023-12-27 13:55:52 +0800

    添加步骤时设置statusCode default:5

--- src/main/java/com/hu/oneclick/server/service/impl/TestCaseStepServiceImpl.java
+++ src/main/java/com/hu/oneclick/server/service/impl/TestCaseStepServiceImpl.java
@@ -48,6 +48,8 @@ public class TestCaseStepServiceImpl extends ServiceImpl<TestCaseStepDao, TestCa
             TestCaseStep testCaseStep = new TestCaseStep();
             BeanUtil.copyProperties(step, testCaseStep);
             testCaseStep.setTestCaseId(dto.getTestCaseId());
+            // 设置statusCode默认值
+            testCaseStep.setStatusCode(5);
             // 保存自定义字段
             if (!JSONUtil.isNull(step.getCustomFieldDatas())) {
                 testCaseStep.setTeststepExpand(JSONUtil.toJsonStr(step.getCustomFieldDatas()));
