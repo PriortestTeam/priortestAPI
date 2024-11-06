9f0d9d3221de7c509d2749abf6c13e0ddf5b953d
ywp <ywp_ywp@126.com>
2024-11-06 11:04:06 +0800

    finish `Issue:#IAWIXG`

--- src/main/java/com/hu/oneclick/server/service/PdfGenerateService.java
+++ src/main/java/com/hu/oneclick/server/service/PdfGenerateService.java
@@ -3,5 +3,5 @@ package com.hu.oneclick.server.service;
 import com.hu.oneclick.model.param.SignOffParam;
 
 public interface PdfGenerateService {
-    void generatePdf(SignOffParam signOffParam);
+    Object generatePdf(SignOffParam signOffParam);
 }
