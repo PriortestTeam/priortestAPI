9ad9ce5254fff025ef934142c5f459eb30f6a268
ywp <ywp_ywp@126.com>
2024-12-05 13:19:27 +0800

    finished issues `#IB3A3K`

--- src/test/java/CustomeFieldsTest.java
+++ src/test/java/CustomeFieldsTest.java
@@ -3,7 +3,9 @@ import cn.hutool.json.JSONObject;
 import cn.hutool.json.JSONUtil;
 import com.hu.oneclick.OneClickApplication;
 import com.hu.oneclick.model.base.Resp;
+import com.hu.oneclick.model.domain.dto.CustomFieldDto;
 import com.hu.oneclick.model.domain.dto.CustomFieldsDto;
+import com.hu.oneclick.model.domain.vo.CustomFileldLinkVo;
 import com.hu.oneclick.server.service.CustomFieldsService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -13,6 +15,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import java.io.File;
 import java.nio.charset.Charset;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @SpringBootTest(classes = OneClickApplication.class)
 @RunWith(SpringRunner.class)
@@ -28,5 +32,19 @@ public class CustomeFieldsTest {
 
         Resp<String> stringResp = customFieldsService.updateValueDropDownBox(bean);
         System.out.println(stringResp.getMsg());
+    }
+
+    @Test
+    public void testGetAllCustomList() {
+        CustomFieldDto customFieldDto = new CustomFieldDto();
+        customFieldDto.setProjectId("1670393286336004097");
+        customFieldDto.setScopeId(3000001L);
+        Resp<List<CustomFileldLinkVo>> allCustomList = customFieldsService.getAllCustomList(customFieldDto);
+        System.out.println(allCustomList.getMsg());
+//        System.out.println(allCustomList.getData());
+        long counted1 = allCustomList.getData().stream().filter(obj -> obj.getType().equals("sCustom")).count();//.forEach(System.out::println);
+        System.out.println(counted1);
+        long counted = allCustomList.getData().stream().filter(obj -> !obj.getType().equals("sCustom")).count();//.forEach(System.out::println);
+        System.out.println(counted);
     }
 }
